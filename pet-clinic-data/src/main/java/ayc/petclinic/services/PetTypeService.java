56e3eeddf28269a3302aaa67e52c5b90b4188f9a
Ayce Ciloglugil <ayceciloglugil@gmail.com>
2018-12-19 12:15:24 +0000

    add PetType and Speciality services

---
+++ pet-clinic-data/src/main/java/ayc/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package ayc.petclinic.services;
+
+import ayc.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/ayc/petclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package ayc.petclinic.services;
+
+import ayc.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+
+}
