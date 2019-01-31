21650425cfce0ec6d9f020724375e48b751240eb
Ayce Ciloglugil <ayceciloglugil@gmail.com>
2019-01-31 16:31:01 +0000

    add json end point to VetController

--- pet-clinic-web/src/main/java/ayc/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ayc/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package ayc.petclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import ayc.petclinic.model.Vet;
 import ayc.petclinic.services.VetService;
 
 @Controller
@@ -19,5 +24,10 @@ public class VetController {
 	public String listVets(Model model) {
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
+	}
+	
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetsJson() {
+		return vetService.findAll();
 	}
 }
